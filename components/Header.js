3e2b67c11714029d208f2a53fe7c221557b5d49b
Aung Chan Myae <aungchanmyae10997@gmail.com>
2025-06-05 17:09:39 +0700

    making movies with api

---
+++ components/Header.js
@@ -0,0 +1,11 @@
+import Link from "next/link";
+
+export default async function Header() {
+  return (
+    <nav className="flex justify-between items-center border rounded border-slate-300 px-4 py-2 my-4">
+      <h1 className="font-bold text-lg">
+        <Link href="/">ChanFlix Movies</Link>
+      </h1>
+    </nav>
+  );
+}
